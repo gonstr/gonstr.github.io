f6d70d677f192616206faafbf01ff0236808bc6d
Tom Beijar Johansson <tom.johansson@schibsted.com>
2024-12-17 09:30:20 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Countdown from 'react-countdown';
+const { DateTime } = require('luxon');
+
+const arrival = DateTime.fromObject(
+  { year: 2024, month: 12, day: 19, hour: 8, minute: 0 },
+  { zone: "CET" } // Set timezone to CET
+);
+
+const Completionist = () => <span>She's Here!</span>;
+
+function App() {
+  return (
+    <div className="App">
+      <div className="background"></div>
+      <div className="header">🌲❤️ Liri goes to Sweden for Christmas! ❤️🌲</div>
+      <Countdown date={arrival}>
+        <Completionist />
+      </Countdown>
+    </div>
+  );
+}
+
+export default App;
