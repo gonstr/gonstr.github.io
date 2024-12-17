7a1515effd1c18f6c988436a647567571746e0e3
Tom Beijar Johansson <tom.johansson@schibsted.com>
2024-12-17 18:20:03 +0100

    === equals

--- liri-sweden/src/App.js
+++ liri-sweden/src/App.js
@@ -20,7 +20,7 @@ function App() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (isWithinHours(arrival, 24)) {
+      if (isWithinHours(arrival, 35)) {
         if (version < 2) {
           createSnow();
           showSnow(true);
@@ -33,18 +33,18 @@ function App() {
     }, [version]);
 
   return (
-    <div className={"App " + (version == 2 ? "background2" : "")}>
-      <div className={"background " + (version == 2 ? "dark" : "")}></div>
+    <div className={"App " + (version === 2 ? "background2" : "")}>
+      <div className={"background " + (version === 2 ? "dark" : "")}></div>
       <div className="snow" id="snow"></div>
       <Countdown date={arrival}/>
       <div className="header">
-        {version == 1 ? "🌲 Welcome back Anna! 🌲" : "🇦🇹 Thanks for showing me Austria! 🇦🇹"}
+        {version === 1 ? "🌲 Welcome back Anna! 🌲" : "🇦🇹 Thanks for showing me Austria! 🇦🇹"}
       </div>
       <div className="text">
-        {version == 1 ? "I'm so excited to spend Christmas and New Year's together with you (and the cats ;). I'm looking forward to some peaceful days filled with relaxation, fresh air, and you!" : "I had a great time in Austria. I loved exploring Altenberg and Linz, eating delicious food, and spending time with you and your family. Thank you for showing me around and making me feel at home."}
+        {version === 1 ? "I'm so excited to spend Christmas and New Year's together with you (and the cats ;). I'm looking forward to some peaceful days filled with relaxation, fresh air, and you!" : "I had a great time in Austria. I loved exploring Altenberg and Linz, eating delicious food, and spending time with you and your family. Thank you for showing me around and making me feel at home."}
       </div>
       <div className="text">
-        {version == 1 ? "I hope you have a safe trip and I can't wait to see you again!" : "I hope you have a safe trip to Sweden and I can't wait to see you again!"}
+        {version === 1 ? "I hope you have a safe trip and I can't wait to see you again!" : "I hope you have a safe trip to Sweden and I can't wait to see you again!"}
       </div>
       <div className="footer">
         Love, Tom ❤️
